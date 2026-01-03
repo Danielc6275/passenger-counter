ce62b414fc8f7b19150d5148c45341fd89f76022
danielc6275 <danielc6275@gmail.com>
2026-01-03 15:05:02 -0500

    FInally, some javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+// document.getElementById("count-er").in
+// ctrl + k + c coments out highlighted code
+
+// let myAge = 22;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+
+// console.log('New assignment');
+// let bonusPoints = 50;
+// bonusPoints = bonusPoints + 50;
+// bonusPoints = bonusPoints - 75;
+// bonusPoints = bonusPoints + 45;
+
+// initialize the count at 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-er in the HTML to reflect the new count
+
+function increment () {
+    console.log("The button was clicked!")
+}
